210383cfc8d64fed4398ee145ea7b7b969bd4e3c
nexthis <pawel.romanowski000@gmail.com>
2025-05-02 01:45:42 +0200

    add  file-s3 support

--- medusa/medusa-config.ts
+++ medusa/medusa-config.ts
@@ -3,7 +3,6 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
-
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     workerMode: process.env.WORKER_MODE as "shared" | "worker" | "server",
@@ -45,7 +44,29 @@ module.exports = defineConfig({
         },
       },
     },
+    {
+      resolve: "@medusajs/medusa/file",
+
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+
+              additional_client_config: {
+                forcePathStyle: process.env.S3_FORCE_PATH_STYLE,
+              },
+            },
+          },
+        ],
+      },
+    },
   ],
-
 });
-
